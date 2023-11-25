dd04db8332b494cea283c034cf52dd747f1b31c9
kitkatayama <9929234+kitkatayama@users.noreply.github.com>
2023-11-25 03:40:31 +0000

    fix post script

--- bin/post.sh
+++ bin/post.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-echo '### score'
-echo
-echo '```'
+echo '## git'
 
-tail -n 4 /tmp/benchout
-
-echo '```'
-echo
-
-echo 'alp'
+git -C /home/isucon/webapp/ log -1
 
 echo ''
 
-git log -1
+echo '## alp'
+
+echo ''
 
 echo '```'
-sudo cat /var/log/nginx/access.log | alp ltsv -m '/api/schedules/[a-zA-Z0-9]+'
+# sudo cat /var/log/nginx/access.log | alp ltsv -m '/api/user/[a-zA-Z0-9]/icon+'
+sudo cat /var/log/nginx/access.log | alp ltsv -m '/api/user/[a-zA-Z0-9]+/icon,/api/livestream/[0-9]+/livecomment,/api/livestream/[0-9]+/reaction,/api/livestream/[0-9]+/ngwords,/api/livestream/[0-9]+/moderate,/api/livestream/[0-9]+/report,/api/livestream/[0-9]+/enter,/api/livestream/[0-9]+/exit,/api/livestream/[0-9]+/statistics,/api/user/[a-zA-Z0-9]+/statistics,/api/user/[a-zA-Z0-9]+/theme' --sort sum -r
 echo '```'
