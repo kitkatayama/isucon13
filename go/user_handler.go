ffd25ce25ad89b0988a5e9b5a5a2b63ecd95c1a2
kitkatayama <9929234+kitkatayama@users.noreply.github.com>
2023-11-25 15:17:28 +0900

    アイコンだけDB変える

--- go/user_handler.go
+++ go/user_handler.go
@@ -7,11 +7,14 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"net"
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
 	"time"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
 	"github.com/gorilla/sessions"
 	"github.com/jmoiron/sqlx"
@@ -85,12 +88,77 @@ type PostIconResponse struct {
 	ID int64 `json:"id"`
 }
 
+func iconConnectDB(logger echo.Logger) (*sqlx.DB, error) {
+	const (
+		networkTypeEnvKey = "ISUCON13_MYSQL_DIALCONFIG_NET"
+		addrEnvKey        = "ISUCON13_MYSQL_DIALCONFIG_ADDRESS"
+		portEnvKey        = "ISUCON13_MYSQL_DIALCONFIG_PORT"
+		userEnvKey        = "ISUCON13_MYSQL_DIALCONFIG_USER"
+		passwordEnvKey    = "ISUCON13_MYSQL_DIALCONFIG_PASSWORD"
+		dbNameEnvKey      = "ISUCON13_MYSQL_DIALCONFIG_DATABASE"
+		parseTimeEnvKey   = "ISUCON13_MYSQL_DIALCONFIG_PARSETIME"
+	)
+
+	conf := mysql.NewConfig()
+
+	// 環境変数がセットされていなかった場合でも一旦動かせるように、デフォルト値を入れておく
+	// この挙動を変更して、エラーを出すようにしてもいいかもしれない
+	conf.Net = "tcp"
+	conf.Addr = net.JoinHostPort("192.168.1.12", "3306")
+	conf.User = "isucon"
+	conf.Passwd = "isucon"
+	conf.DBName = "isupipe"
+	conf.ParseTime = true
+
+	if v, ok := os.LookupEnv(networkTypeEnvKey); ok {
+		conf.Net = v
+	}
+	if addr, ok := os.LookupEnv(addrEnvKey); ok {
+		if port, ok2 := os.LookupEnv(portEnvKey); ok2 {
+			conf.Addr = net.JoinHostPort(addr, port)
+		} else {
+			conf.Addr = net.JoinHostPort(addr, "3306")
+		}
+	}
+	if v, ok := os.LookupEnv(userEnvKey); ok {
+		conf.User = v
+	}
+	if v, ok := os.LookupEnv(passwordEnvKey); ok {
+		conf.Passwd = v
+	}
+	if v, ok := os.LookupEnv(dbNameEnvKey); ok {
+		conf.DBName = v
+	}
+	if v, ok := os.LookupEnv(parseTimeEnvKey); ok {
+		parseTime, err := strconv.ParseBool(v)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse environment variable '%s' as bool: %+v", parseTimeEnvKey, err)
+		}
+		conf.ParseTime = parseTime
+	}
+
+	db, err := sqlx.Open("mysql", conf.FormatDSN())
+	if err != nil {
+		return nil, err
+	}
+	db.SetMaxOpenConns(10)
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
 func getIconHandler(c echo.Context) error {
 	ctx := c.Request().Context()
 
 	username := c.Param("username")
 
+	conn, err := iconConnectDB(c.Logger())
+	dbConn = conn
 	tx, err := dbConn.BeginTxx(ctx, nil)
+
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "failed to begin transaction: "+err.Error())
 	}
@@ -118,6 +186,10 @@ func getIconHandler(c echo.Context) error {
 
 func postIconHandler(c echo.Context) error {
 	ctx := c.Request().Context()
+
+	conn, err := iconConnectDB(c.Logger())
+	dbConn = conn
+	tx, err := dbConn.BeginTxx(ctx, nil)
 
 	if err := verifyUserSession(c); err != nil {
 		// echo.NewHTTPErrorが返っているのでそのまま出力
